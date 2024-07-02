dd1c004503cf05a57804d9b4f7f457f49ad23316
prmoore77 <prmoore77@hotmail.com>
2024-07-02 10:35:04 -0400

    bump version 0.0.9 -> 0.0.10

--- src/sqlalchemy_flight_sql_adbc_dialect/__init__.py
+++ src/sqlalchemy_flight_sql_adbc_dialect/__init__.py
@@ -11,7 +11,7 @@ from .sqlalchemy_interfaces import ReflectedColumn, ReflectedPrimaryKeyConstrain
 from sqlalchemy.engine.url import URL
 
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 
 if TYPE_CHECKING:
     from sqlalchemy.base import Connection
