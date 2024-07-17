d5b03c2233ae252e8faef83762e22ed302547b5c
prmoore77 <prmoore77@hotmail.com>
2024-07-17 10:24:57 -0400

    Updated Arrow version to 17, and added STRUCT (JSON) type support

--- src/sqlalchemy_flight_sql_adbc_dialect/__init__.py
+++ src/sqlalchemy_flight_sql_adbc_dialect/__init__.py
@@ -305,6 +305,8 @@ class FlightSQLDialect(DefaultDialect):
             return sqltypes.Float
         elif data_type == "BOOLEAN":
             return sqltypes.Boolean
+        elif re.match(pattern="^STRUCT\(", string=data_type):
+            return sqltypes.JSON
         else:
             # Try a catch-all for any other data types
             try:
