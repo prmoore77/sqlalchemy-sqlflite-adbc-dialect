3b5ea3f70360dce72432aee012215ec3f81ed19b
Philip Moore <prmoore77@hotmail.com>
2024-10-21 14:37:38 -0400

    bump version 0.0.20 -> 0.0.21

--- src/sqlalchemy_gizmosql_adbc_dialect/__init__.py
+++ src/sqlalchemy_gizmosql_adbc_dialect/__init__.py
@@ -13,7 +13,7 @@ from sqlalchemy.engine.url import URL
 from adbc_driver_manager import OperationalError
 
 
-__version__ = "0.0.20"
+__version__ = "0.0.21"
 
 if TYPE_CHECKING:
     from sqlalchemy.base import Connection
